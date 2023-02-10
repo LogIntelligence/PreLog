4fd3899047e48f8a977ec429bc127b8190e43347
vanhoanglepsa <82471338+vanhoanglepsa@users.noreply.github.com>
2023-02-10 22:11:46 +1100

    Update __init__.py

--- openprompt/__init__.py
+++ openprompt/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.0.1"
+__version__ = "1.0.1beta"
 from .pipeline_base import PromptDataLoader, PromptModel, PromptForClassification, PromptForGeneration
 from .utils import *
 from .prompt_base import Template, Verbalizer
